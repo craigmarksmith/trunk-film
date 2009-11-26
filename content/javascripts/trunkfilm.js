94343978d914e9700d290999c819f2cf6eeabaf6
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2009-11-26 22:25:56 +0000

    scrolling works. I rock

--- content/javascripts/trunkfilm.js
+++ content/javascripts/trunkfilm.js
@@ -1,1 +1,35 @@
+TrunkFilm = {}
+TrunkFilm.Movies = {
+  amount_to_move: 900,
 
+  init: function(){
+    current_position = 0;
+    $('#scroll-left').click(TrunkFilm.Movies.scrollLeft);
+    $('#scroll-right').click(TrunkFilm.Movies.scrollRight);
+  },
+
+  scrollLeft: function(){
+    if(current_position > 0){
+      TrunkFilm.Movies.scroll('+='+TrunkFilm.Movies.amount_to_move+'px');
+      current_position -= 1;
+    }
+    return false;
+  },
+
+  scrollRight: function(){
+    if(current_position < ($('.movie').size()-1)){
+      TrunkFilm.Movies.scroll('-='+TrunkFilm.Movies.amount_to_move+'px');
+      current_position += 1;
+    }
+    return false;
+  },
+
+  scroll: function(scroll_amount){
+    $('#movies').animate({
+      marginLeft: scroll_amount
+    }, 1000);
+  }
+
+}
+
+$(document).ready(TrunkFilm.Movies.init);

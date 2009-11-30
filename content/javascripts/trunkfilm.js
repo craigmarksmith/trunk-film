7c529409fbf1fb027773b4af7d3738946a9a48e1
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2009-11-30 22:15:38 +0000

    about us scroll working

--- content/javascripts/trunkfilm.js
+++ content/javascripts/trunkfilm.js
@@ -1,30 +1,34 @@
-TrunkFilm = {}
-TrunkFilm.Movies = {
-  amount_to_move: 900,
-
+TrunkFilm = {
   init: function(){
-    current_position = 0;
-    $('#scroll-left').click(TrunkFilm.Movies.scrollLeft);
-    $('#scroll-right').click(TrunkFilm.Movies.scrollRight);
     $('#contact-details').hide();
     $('#contact a').click(function(){
       $('#contact-details').toggle();
       return false;
     });
+  }
+},
+
+TrunkFilm.Movies = {
+  amount_to_move: 900,
+  current_position: 0,
+
+  init: function(){
+    $('#scroll-left').click(TrunkFilm.Movies.scrollLeft);
+    $('#scroll-right').click(TrunkFilm.Movies.scrollRight);
   },
 
   scrollLeft: function(){
-    if(current_position > 0){
+    if(TrunkFilm.Movies.current_position > 0){
       TrunkFilm.Movies.scroll('+='+TrunkFilm.Movies.amount_to_move+'px');
-      current_position -= 1;
+      TrunkFilm.Movies.current_position--;
     }
     return false;
   },
 
   scrollRight: function(){
-    if(current_position < ($('.movie').size()-1)){
+    if(TrunkFilm.Movies.current_position < ($('.movie').size()-1)){
       TrunkFilm.Movies.scroll('-='+TrunkFilm.Movies.amount_to_move+'px');
-      current_position += 1;
+      TrunkFilm.Movies.current_position++;
     }
     return false;
   },
@@ -35,6 +39,51 @@ TrunkFilm.Movies = {
     }, 1000);
   }
 
+},
+
+TrunkFilm.AboutUs = {
+
+  current_position: 0,
+  amount_to_move: 655,
+
+  init: function(){
+    $('#scroll-left').hide();
+    $('#scroll-right').click(TrunkFilm.AboutUs.scrollRight);
+    $('#scroll-left').click(TrunkFilm.AboutUs.scrollLeft);
+  },
+
+  scrollLeft: function(){
+    TrunkFilm.AboutUs.current_position--;
+    TrunkFilm.AboutUs.scroll('+');
+    return false;
+  },
+
+  scrollRight: function(){
+    TrunkFilm.AboutUs.current_position++;
+    TrunkFilm.AboutUs.scroll('-');
+    return false;
+  },
+  
+  scroll: function(direction){
+    $('div#about-us img').animate({
+      marginLeft: direction+'='+TrunkFilm.AboutUs.amount_to_move+'px'
+    }, 1000);
+
+    $('#texts').animate({
+      marginLeft: direction+'='+TrunkFilm.AboutUs.amount_to_move+'px'
+    }, 1000);
+
+    if(TrunkFilm.AboutUs.current_position == 0){
+      $('#scroll-left').hide();
+    } else if($('.text').size()-1 == TrunkFilm.AboutUs.current_position){
+      $('#scroll-right').hide();
+    } else {
+      $('#scroll-left').show();
+      $('#scroll-right').show();
+    }
+  }
 }
 
-$(document).ready(TrunkFilm.Movies.init);
+$(document).ready(function(){
+  TrunkFilm.init();
+});
